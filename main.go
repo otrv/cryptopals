ead6c44a456b9227cfb64940c0aa27e22bd22d4d
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2026-02-23 22:46:56 +0300

    1-4

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"math"
+	"os"
 )
 
 func fixedXor(a []byte, b []byte) ([]byte, error) {
@@ -21,68 +21,81 @@ func fixedXor(a []byte, b []byte) ([]byte, error) {
 	return res, nil
 }
 
-func chi2(observed float64, expected float64) float64 {
-	diff := float64(observed) - expected
-	return (diff * diff) / expected
-}
-
-// minFloat64 return the minimum value and its index from an array of float64
-func minFloat64(arr []float64) (float64, int) {
-	minValue := arr[0]
-	minIndex := 0
+func maxFloat64(arr []float64) int {
+	maxIndex := 0
+	maxValue := 0.0
 	for i, score := range arr {
-		if score < minValue {
-			minValue = score
-			minIndex = i
+		if score > maxValue {
+			maxValue = score
+			maxIndex = i
 		}
 	}
 
-	return minValue, minIndex
+	return maxIndex
 }
 
-// determineEnglishness determine how closer a given sequence of byte to plaintext english. Lower is better
-func determineEnglishness(in []byte) float64 {
-	engFreqs := [27]float64{
-		0.0651738, 0.0124248, 0.0217339, 0.0349835,
-		0.1041442, 0.0197881, 0.0158610, 0.0492888,
-		0.0558094, 0.0009033, 0.0050529, 0.0331490,
-		0.0202124, 0.0564513, 0.0596302, 0.0137645,
-		0.0008606, 0.0497563, 0.0515760, 0.0729357,
-		0.0225134, 0.0082903, 0.0171272, 0.0013692,
-		0.0145984, 0.0007836, 0.1918182,
+func scoreEnglishness(in []byte) float64 {
+	letterScores := map[byte]float64{
+		'A': 8.167,
+		'B': 1.492,
+		'C': 2.782,
+		'D': 4.253,
+		'E': 12.702,
+		'F': 2.228,
+		'G': 2.015,
+		'H': 6.094,
+		'I': 6.966,
+		'J': 0.153,
+		'K': 0.772,
+		'L': 4.025,
+		'M': 2.406,
+		'N': 6.749,
+		'O': 7.507,
+		'P': 1.929,
+		'Q': 0.095,
+		'R': 5.987,
+		'S': 6.327,
+		'T': 9.056,
+		'U': 2.758,
+		'V': 0.978,
+		'W': 2.360,
+		'X': 0.150,
+		'Y': 1.974,
+		'Z': 0.074,
+		' ': 15.000,
 	}
 
-	var occurences [27]int
-	validSize := 0
+	score := 0.0
 
 	for _, ch := range in {
-		if ch == byte(' ') {
-			occurences[26]++ // 26 is space
-			validSize++
-		} else if ch >= byte('A') && ch <= byte('Z') {
-			occurences[ch-byte('A')]++
-			validSize++
-		} else if ch >= byte('a') && ch <= byte('z') {
-			occurences[ch-32-byte('A')]++
-			validSize++
+		if ch >= byte('a') && ch <= byte('z') {
+			ch = ch - 32
 		}
-	}
 
-	if validSize == 0 {
-		return math.Inf(1)
-	}
-
-	var score float64 = 0
-	for i, observed := range occurences {
-		expected := float64(validSize) * engFreqs[i]
-		score += chi2(float64(observed), expected)
+		val, ok := letterScores[ch]
+		if ok {
+			score += val
+		} else if ch < 32 || ch > 126 {
+			score -= 20
+		}
 	}
 
 	return score
 }
 
+func findMostEnglish(in [][]byte) int {
+	scores := make([]float64, len(in))
+	for idx, candidate := range in {
+		scores[idx] = scoreEnglishness(candidate)
+	}
+
+	idx := maxFloat64(scores[:])
+
+	return idx
+}
+
 func decryptSingleByteXor(in []byte) []byte {
-	var candidates [255][]byte
+	var candidates [256][]byte
 	for i := range 255 {
 		b := bytes.Repeat([]byte{byte(i)}, len(in))
 
@@ -93,21 +106,30 @@ func decryptSingleByteXor(in []byte) []byte {
 		candidates[i] = candidate
 	}
 
-	var scores [255]float64
-	for idx, candidate := range candidates {
-		scores[idx] = determineEnglishness(candidate)
-	}
-
-	_, idx := minFloat64(scores[:])
+	idx := findMostEnglish(candidates[:])
 
 	return candidates[idx]
 }
 
 func main() {
-	crypted, err := hex.DecodeString("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	file, err := os.ReadFile("./input.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(decryptSingleByteXor(crypted)))
+	lines := bytes.Split(file, []byte{'\n'})
+
+	decryptedLines := make([][]byte, len(lines))
+	for idx, line := range lines {
+		decoded, err := hex.DecodeString(string(line))
+		if err != nil {
+			panic(err)
+		}
+		decryptedLines[idx] = decryptSingleByteXor(decoded)
+		fmt.Println(string(decryptedLines[idx]))
+	}
+
+	topIdx := findMostEnglish(decryptedLines)
+
+	fmt.Println(string(decryptedLines[topIdx]))
 }
